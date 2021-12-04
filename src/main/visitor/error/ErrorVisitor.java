24250ff30b9aa6dd2af15ec1eff441a14a00b845
bahjat <mortezabahjat@gmail.com>
2021-12-04 20:00:17 +0330

    Error Level 1

--- src/main/visitor/error/ErrorVisitor.java
+++ src/main/visitor/error/ErrorVisitor.java
@@ -5,19 +5,17 @@ import main.ast.nodes.declaration.FunctionDeclaration;
 import main.ast.nodes.declaration.MainDeclaration;
 import main.ast.nodes.declaration.VariableDeclaration;
 import main.ast.nodes.declaration.struct.StructDeclaration;
-import main.ast.nodes.expression.*;
-import main.ast.nodes.expression.values.primitive.BoolValue;
-import main.ast.nodes.expression.values.primitive.IntValue;
+import main.ast.nodes.expression.FunctionCall;
 import main.ast.nodes.statement.*;
 import main.compileError.CompileError;
-import main.compileError.nameError.DuplicateFunction;
-import main.compileError.nameError.DuplicateStruct;
-import main.compileError.nameError.FunctionStructConflict;
+import main.compileError.nameError.*;
 import main.symbolTable.SymbolTable;
 import main.symbolTable.exceptions.ItemAlreadyExistsException;
 import main.symbolTable.exceptions.ItemNotFoundException;
 import main.symbolTable.items.FunctionSymbolTableItem;
 import main.symbolTable.items.StructSymbolTableItem;
+import main.symbolTable.items.VariableSymbolTableItem;
+import main.symbolTable.utils.Stack;
 import main.visitor.Visitor;
 
 import java.util.UUID;
@@ -33,6 +31,7 @@ public class ErrorVisitor extends Visitor<Void> {
     @Override
     public Void visit(Program program) {
         SymbolTable.root = new SymbolTable();
+        SymbolTable.push(SymbolTable.root);
 
         for (StructDeclaration struct : program.getStructs()) {
             var structItem = new StructSymbolTableItem(struct);
@@ -55,62 +54,70 @@ public class ErrorVisitor extends Visitor<Void> {
         }
 
         for (FunctionDeclaration function : program.getFunctions()) {
-            var funcItem = new FunctionSymbolTableItem(function);
-            SymbolTable.push(new SymbolTable(SymbolTable.root));
-            funcItem.setFunctionSymbolTable(SymbolTable.top);
-
-            try {
-                SymbolTable.root.getItem(StructSymbolTableItem.START_KEY + funcItem.getName());
-                errorPrinter(new FunctionStructConflict(function.getLine(), funcItem.getName()));
-            } catch (ItemNotFoundException ignored) {}
-
-            try {
-                SymbolTable.root.put(funcItem);
-            } catch (ItemAlreadyExistsException e) {
-                errorPrinter(new DuplicateFunction(function.getLine(), funcItem.getName()));
-                funcItem.setName(UUID.randomUUID().toString());
-                try {
-                    SymbolTable.root.put(funcItem);
-                } catch (ItemAlreadyExistsException ex) {
-                    ex.printStackTrace();
-                }
-            }
             function.accept(this);
-            SymbolTable.pop();
         }
-
-        var main = new FunctionDeclaration();
-        main.setBody(program.getMain().getBody());
-        var funcItem = new FunctionSymbolTableItem(main);
-        SymbolTable.push(new SymbolTable(SymbolTable.root));
-        funcItem.setFunctionSymbolTable(SymbolTable.top);
-
-        try {
-            SymbolTable.root.put(funcItem);
-        } catch (ItemAlreadyExistsException ignored) {
-        }
-
         program.getMain().accept(this);
-        SymbolTable.pop();
         return super.visit(program);
     }
 
     @Override
     public Void visit(FunctionDeclaration functionDeclaration) {
+        var funcItem = new FunctionSymbolTableItem(functionDeclaration);
+        SymbolTable.push(new SymbolTable(SymbolTable.top));
+        funcItem.setFunctionSymbolTable(SymbolTable.top);
+
+        try {
+            SymbolTable.top.pre.getItem(StructSymbolTableItem.START_KEY + funcItem.getName());
+            errorPrinter(new FunctionStructConflict(functionDeclaration.getLine(), funcItem.getName()));
+        } catch (ItemNotFoundException ignored) {}
+
+        try {
+            SymbolTable.top.pre.put(funcItem);
+        } catch (ItemAlreadyExistsException e) {
+            errorPrinter(new DuplicateFunction(functionDeclaration.getLine(), funcItem.getName()));
+            funcItem.setName(UUID.randomUUID().toString());
+            try {
+                SymbolTable.top.pre.put(funcItem);
+            } catch (ItemAlreadyExistsException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         for (VariableDeclaration arg : functionDeclaration.getArgs())
             arg.accept(this);
         functionDeclaration.getBody().accept(this);
+
+        SymbolTable.pop();
         return super.visit(functionDeclaration);
     }
 
     @Override
     public Void visit(MainDeclaration mainDeclaration) {
-        mainDeclaration.getBody().accept(this);
-        return super.visit(mainDeclaration);
+        var functionDeclaration = new FunctionDeclaration();
+        functionDeclaration.setBody(mainDeclaration.getBody());
+        return super.visit(functionDeclaration);
     }
 
     @Override
     public Void visit(VariableDeclaration variableDeclaration) {
+        var varItem = new VariableSymbolTableItem(variableDeclaration.getVarName());
+        try {
+            SymbolTable.top.getItem(StructSymbolTableItem.START_KEY+varItem.getName());
+            errorPrinter(new VarStructConflict(variableDeclaration.getLine(), varItem.getName()));
+        } catch (ItemNotFoundException ignored) {
+        }
+        try {
+            SymbolTable.top.getItem(FunctionSymbolTableItem.START_KEY+varItem.getName());
+            errorPrinter(new VarFunctionConflict(variableDeclaration.getLine(), varItem.getName()));
+        } catch (ItemNotFoundException ignored) {
+        }
+
+        try {
+            SymbolTable.top.put(varItem);
+        } catch (ItemAlreadyExistsException ignored) {
+            errorPrinter(new DuplicateVar(variableDeclaration.getLine(), varItem.getName()));
+        }
+
         return super.visit(variableDeclaration);
     }
 
@@ -126,16 +133,30 @@ public class ErrorVisitor extends Visitor<Void> {
 
     @Override
     public Void visit(BlockStmt blockStmt) {
+        for (Statement statement : blockStmt.getStatements()) {
+            statement.accept(this);
+        }
         return super.visit(blockStmt);
     }
 
     @Override
     public Void visit(ConditionalStmt conditionalStmt) {
+
+        SymbolTable.push(new SymbolTable(SymbolTable.top));
+        conditionalStmt.getThenBody().accept(this);
+        SymbolTable.pop();
+        SymbolTable.push(new SymbolTable(SymbolTable.top));
+        conditionalStmt.getElseBody().accept(this);
+        SymbolTable.pop();
+
         return super.visit(conditionalStmt);
     }
 
     @Override
     public Void visit(LoopStmt loopStmt) {
+        SymbolTable.push(new SymbolTable(SymbolTable.top));
+        loopStmt.getBody().accept(this);
+        SymbolTable.pop();
         return super.visit(loopStmt);
     }
 }
